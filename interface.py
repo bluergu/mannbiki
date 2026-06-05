fcddea0f8df54d8986dcb00df1cbb0a10055597b
bluergu <shu.mimi1980@icloud.com>
2026-06-05 06:11:15 +0000

    feat: ボットの性格を〇〇に変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "語尾に「haihai」をつけます"},
+    {"role": "system", "content": "語尾に「ぴえん」をつけます"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
